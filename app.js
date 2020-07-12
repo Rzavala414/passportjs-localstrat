e1a9a97bcb2c23d6acb572e5dc89db4f33a9fcb1
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-12 14:37:53 -0700

    created new views files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
 
 //routes
 app.use('/', require('./routes/index'));
