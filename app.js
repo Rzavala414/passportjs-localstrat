54b0a8a1006cc77346d0314c67e2e8bbec08aebd
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-13 10:37:52 -0700

    created new user schema model

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const db = require('./config/keys').MONGO_URI
 const app = express();
+
+//database
+mongoose.connect(db, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}, console.log('connected to db'))
+    
+   
 
 const PORT = process.env.PORT || 3000;
 
+//view engine ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs')
+
+//express body parser
+app.use(express.urlencoded({extended: false}));
 
 //routes
 app.use('/', require('./routes/index'));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://roger:roger123@local-strat.f1qxi.mongodb.net/local-strat?retryWrites=true&w=majority'
+}
